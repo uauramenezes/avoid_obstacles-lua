551b0978b78153a9ec1be6a7e8b8bf13c968ec93
JouChark <JouChark@users.noreply.github.com>
2020-09-14 00:20:26 -0300

    add basic configuration

--- main.lua
+++ main.lua
@@ -2,7 +2,14 @@ WIDTH = 800
 HEIGHT = 600
 
 function love.load()
-    
+    -- seed the RNG so that calls to random are always random
+    math.randomseed(os.time())
+
+    -- initialize text fonts
+    font = love.graphics.newFont('mono.ttf', 15)
+    love.graphics.setFont(font)
+
+    love.window.setMode(WIDTH, HEIGHT, {resizable=true, vsync=false, fullscreen=false})
 end
 
 function love.update()
