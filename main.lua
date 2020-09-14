2cce8253915de84c7216fef3586ee134ccf3a55d
JouChark <JouChark@users.noreply.github.com>
2020-09-14 00:23:31 -0300

    add elements

--- main.lua
+++ main.lua
@@ -10,6 +10,43 @@ function love.load()
     love.graphics.setFont(font)
 
     love.window.setMode(WIDTH, HEIGHT, {resizable=true, vsync=false, fullscreen=false})
+
+    -- function to create walls objects
+    function createWalls(x)
+        local this = 
+        {
+            x = x,
+            y = 0,
+            width = 4,
+            height = HEIGHT
+        }
+
+        return this
+    end
+
+    -- function to create elements objects
+    function createElements(x, y, line)
+        local this = 
+        {
+            x = x,
+            y = y,
+            width = 50,
+            height = 50,
+            line = line
+        }
+
+        return this
+    end
+
+    -- create the walls, the player and obstacles 
+    wallLeft = createWalls(200)
+    wallRight = createWalls(600)
+
+    player = createElements(WIDTH / 2 - 25, 525, 1)
+
+    obstacle1 = createElements(250, 300, 1)
+    obstacle2 = createElements(350, 300, 2)
+    obstacle3 = createElements(450, 300, 3)
 end
 
 function love.update()
