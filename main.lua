38211043a1232b24721d57f73919387f4940fb32
Uaurá Menezes <uaurasilva@hotmail.com>
2020-09-14 23:28:21 -0300

    add messages for each game state

--- main.lua
+++ main.lua
@@ -3,10 +3,10 @@ HEIGHT = 550
 
 function love.load()
     -- initialize text fonts
-    font = love.graphics.newFont('mono.ttf', 15)
+    font = love.graphics.newFont(30)
     love.graphics.setFont(font)
 
-    love.window.setMode(WIDTH, HEIGHT, {resizable=true, vsync=false, fullscreen=false})
+    love.window.setMode(WIDTH, HEIGHT, {resizable=false, vsync=false, fullscreen=false})
 
     -- function to create walls objects
     function createWalls(x)
@@ -56,10 +56,10 @@ function love.load()
     end
 
     -- create the walls, the player and obstacles 
-    wallLeft = createWalls(WIDTH / 4 - 2)
-    wallRight = createWalls(WIDTH * (3 / 4) - 2)
+    leftWall = createWalls(WIDTH / 4 - 2)
+    rightWall = createWalls(WIDTH * (3 / 4) - 2)
 
-    player = createElements(WIDTH / 2 - 25, 475, 1)
+    player = createElements(WIDTH / 2 - 25, HEIGHT - 51, 1)
 
     obstacle = {}
     for i = 1, 5, 1 do
@@ -70,21 +70,36 @@ function love.load()
         end
     end
 
+    gameState = 'start'
+    score = 0
 end
 
 function love.update()
 
 end
 
+
+
 function love.draw()
     love.graphics.rectangle('fill', player.x, player.y, player.width, player.height)
 
     
-    love.graphics.rectangle('fill', wallLeft.x, wallLeft.y, wallLeft.width, wallLeft.height)
-    love.graphics.rectangle('fill', wallRight.x, wallRight.y, wallRight.width, wallRight.height)
+    love.graphics.rectangle('fill', leftWall.x, leftWall.y, leftWall.width, leftWall.height)
+    love.graphics.rectangle('fill', rightWall.x, rightWall.y, rightWall.width, rightWall.height)
 
-    for i = 1, 5, 1 do
-        love.graphics.rectangle('fill', obstacle[i].x, obstacle[i].y, obstacle[i].width, obstacle[i].height)
+    love.graphics.printf('Score', 0, 75, leftWall.x, 'center')
+    love.graphics.printf(score, 0, 125, leftWall.x, 'center')
+
+    if gameState == 'play' then
+        love.graphics.printf('Press Space', rightWall.x + 2, 75, WIDTH - rightWall.x, 'center')
+    elseif gameState == 'start' then
+        love.graphics.printf('Press Enter to start!', 0, 125, WIDTH, 'center')
+        love.graphics.printf('Press Space to pause!', 0, 175, WIDTH, 'center')
+        love.graphics.printf('Press Enter', rightWall.x + 2, 75, WIDTH - rightWall.x, 'center')
+    elseif gameState == 'gameOver' then
+        love.graphics.printf('Press Escape to restart!', 0, 50, WIDTH, 'center')
+        love.graphics.printf('Press Esc', rightWall.x + 2, 75, WIDTH - rightWall.x, 'center')
     end
 
+    
 end
